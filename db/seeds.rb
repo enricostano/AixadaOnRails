48af42bab0a38591e834a75abdfddbe8385d0b19
enricostano <ocirne.onats@gmail.com>
2012-11-16 13:53:23 +0100

    fixing seeds...

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,19 @@ User.create([ email: 'puppa@puppa.pup',
               password: 'password',
               password_confirmation: 'password'])
 
-Member.create([ name: 'admin',
-                user: User.first,
+FamilyUnit.create([ name: 'admin' ])
+
+Member.create([ name:              'admin',
+                user:              User.first,
+                family_unit:       FamilyUnit.first,
                 custom_member_ref: 'que es esto',
-                address: 'c/ de la Aixada, 34',
-                nif: '12345678901',
-                zip: '74024',
-                city: 'Barcelona' ])
+                address:           'c/ de la Aixada, 34',
+                nif:               '12345678901',
+                zip:               '74024',
+                city:              'Barcelona' ])
 
-FamilyUnit.create([ name: 'admin',
-                    member: Member.first ])
 
-Incident.create([ subject: 'My first ever incident',
+Incident.create([ subject:       'My first ever incident',
                   incident_type: IncidentType.first,
-                  user: User.first,
-                  details: 'Vaya asco de zanahorias...' ])
+                  user:          User.first,
+                  details:       'Vaya asco de zanahorias...' ])
